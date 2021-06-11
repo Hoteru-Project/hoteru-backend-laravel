35f2291eeacca40c630e16621111890eb59e95c8
MohammedHieba <mohamedhieba9574@gmail.com>
2021-06-11 20:10:11 +0200

    Add admin panel and create roles.

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
