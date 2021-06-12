e37e0cdd71b34c6f0a8c945c322e22009ec5fb22
Malik Hesham <malikhesham@outlook.com>
2021-06-12 14:23:49 +0200

    making filter case insensitive

--- app/Services/V1/FilterService.php
+++ app/Services/V1/FilterService.php
@@ -15,8 +15,9 @@ class FilterService implements Filter {
     }
 
     public function filterHotels($filterParams) {
+
         for ($i = 0; $i < count($this->receivedHotels); $i++) {
-            if ($this->checkRequiredFilter($this->receivedHotels[$i]->mainAmenities, $filterParams)) {
+            if ($this->checkRequiredFilter($this->ignoreCase($this->receivedHotels[$i]->mainAmenities), $filterParams)) {
                 array_push($this->filteredHotels, $this->receivedHotels[$i]);
             }
         }
@@ -24,16 +25,27 @@ class FilterService implements Filter {
 
     public function checkRequiredFilter($filteringFeatures, $filterParams) {
         $filterParams = explode('&', $filterParams);
-
+        $filterParams =$this->ignoreCase($filterParams);
         if (!empty(array_intersect($filteringFeatures, $filterParams))) return true;
         return false;
+    }
+
+    public function ignoreCase($filterParams) {
+        $j = 0;
+        foreach( $filterParams as $element ) {
+            $filterParams[$j] = strtolower($element);
+            $j++;
+        }
+        return $filterParams;
     }
 
     public function getFilteredHotels() {
         return $this->filteredHotels;
     }
+
     // Test URLs => {
     //    http://127.0.0.1:8001/api/v1/hotels/alexandria/filter=POOL
+    //    http://127.0.0.1:8001/api/v1/hotels/alexandria/filter=pool&WIFI
     //    http://127.0.0.1:8001/api/v1/hotels/alexandria/filter=pool
     //    http://127.0.0.1:8001/api/v1/hotels/alexandria/filter=pool&wifi
     //    }
