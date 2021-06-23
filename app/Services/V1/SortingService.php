76303e0bab25b5780b17485f440cc425b104b7cd
Malik Hesham <malikhesham@outlook.com>
2021-06-23 02:15:19 +0200

    fixing sorting bug

--- app/Services/V1/SortingService.php
+++ app/Services/V1/SortingService.php
@@ -3,12 +3,6 @@
 
 namespace App\Services\V1;
 
-use App\Services\V1\FormatterService;
-use App\Services\V1\SearchService;
-use App\Services\V1\FilterService;
-use Illuminate\Support\Facades\Request;
-
-
 class SortingService {
     protected array $receivedHotels;
     protected int $sortingID;
@@ -18,50 +12,26 @@ class SortingService {
         $this->sortingID = $sentID;
     }
 
-
     public function sortSentHotels() {
         $sortMode = $this->sortingID;
-        $unSortedHotels = $this->receivedHotels;
-        $sortedHotels = [];
+        $fetchedHotels = $this->receivedHotels;
+
         switch ($sortMode) {
-            case 1:
-                $sortedHotels = $this->sortByPricing($unSortedHotels);
+            case "1":
+                usort($fetchedHotels,function($firstHotel, $secondHotel)  {
+                    return $firstHotel->hotelPricing->startingAt->plain > $secondHotel->hotelPricing->startingAt->plain;
+                });
+
                 break;
-            case 2:
-                $sortedHotels = $this->sortByRating($unSortedHotels);
+            case "2":
+                usort($fetchedHotels,function($firstHotel, $secondHotel)  {
+                    return $firstHotel->guestReviews->overallRating < $secondHotel->guestReviews->overallRating;
+                });
                 break;
             case 3:
                 break;
         }
-        return $sortedHotels;
-    }
-
-    public function sortByPricing($hotels) {
-
-        for($i=0;$i<count($hotels);$i++) {
-            $val = $hotels[$i]->hotelPricing->startingAt->plain;
-            $j = $i-1;
-            while($j>=0 && $hotels[$j]->hotelPricing->startingAt->plain > $val){
-                $hotels[$j+1]->hotelPricing->startingAt->plain = $hotels[$j]->hotelPricing->startingAt->plain;
-                $j--;
-            }
-            $hotels[$j+1]->hotelPricing->startingAt->plain = $val;
-        }
-        return $hotels;
-    }
-
-    public function sortByRating($hotels) {
-
-        for($i=0;$i<count($hotels);$i++) {
-            $val = $hotels[$i]->guestReviews->overallRating;
-            $j = $i-1;
-            while($j>=0 && $hotels[$j]->guestReviews->overallRating < $val){
-                $hotels[$j+1]->guestReviews->overallRating = $hotels[$j]->guestReviews->overallRating;
-                $j--;
-            }
-            $hotels[$j+1]->guestReviews->overallRating = $val;
-        }
-        return $hotels;
+        return $fetchedHotels;
     }
 
 }
